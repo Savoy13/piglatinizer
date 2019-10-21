f64aed6347627a610b3e164a4dc0548db5f92505
Savoy13 <savoy.tyauhn13@gmail.com>
2019-10-21 22:05:31 +0000

    10/21

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
-$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$( "document" ).ready(function() {
+    $("button").click(function(){
+        $(".output").text("");
+        var sentence =$("input").val();
+        var words = sentence.split(' ');
+        var vowels = [ 'a', 'e', 'i', 'o', 'u' ];
+        words.forEach(function(word){
+            var firstLetter=word[0].toLowerCase();
+            if (vowels.includes(firstLetter)) {
+                console.log(true);
+                var vowelWord= word + "ay ";
+                $('.output').append(vowelWord);
+            }else {
+                  console.log(false);
+                var conWord= word.slice(1) + firstLetter + "ay ";
+                $('.output').append(conWord);
 
+                                            }
+        });
 
+    });
 
-  
-  
-  
-  
 });
+
+
+
+
+
+
+
+
